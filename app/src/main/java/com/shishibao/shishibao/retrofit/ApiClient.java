e939ddf057e44663c94072168894d0538c197906
xczhenhk@163.com <jiliang222@163.com>
2018-07-15 03:35:03 +0800

    请求后台

---
+++ app/src/main/java/com/shishibao/shishibao/retrofit/ApiClient.java
@@ -0,0 +1,25 @@
+package com.shishibao.shishibao.retrofit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    public static Retrofit mRetrofit;
+
+    public static Retrofit retrofit() {
+        if (mRetrofit == null) {
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            OkHttpClient okHttpClient = builder.build();
+            mRetrofit = new Retrofit.Builder()
+                    .baseUrl(ApiService.API_SERVER_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                    .client(okHttpClient)
+                    .build();
+        }
+        return mRetrofit;
+    }
+
+}
--- app/src/main/java/com/shishibao/shishibao/view/MainActivity.java
+++ app/src/main/java/com/shishibao/shishibao/view/MainActivity.java
@@ -1,7 +1,8 @@
-package com.shishibao.shishibao;
+package com.shishibao.shishibao.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,16 +11,28 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.alibaba.fastjson.JSON;
+import com.orhanobut.logger.Logger;
+import com.shishibao.shishibao.R;
+import com.shishibao.shishibao.presenter.RowPresenter;
+
+import model.RoomsModel;
+
+public class MainActivity extends AppCompatActivity  implements RoomsView{
 
     private ListView listView;
+    private RowPresenter rowPresenter = new RowPresenter(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("dd", "<<<<<>>>>>>>>>1");
+        Log.d("dd", "<<<<<>>>>>>>>>3");
+        rowPresenter.loadDataByRetrofitRxjava();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = (ListView) findViewById(R.id.listView1);
         listView.setAdapter(new MyAdapter());
+        Log.d("dd","<<<<<>>>>>>>>>2");
     }
 
     // 定义的数据
@@ -27,23 +40,28 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.vae, R.drawable.vae, R.drawable.vae, R.drawable.vae,
             R.drawable.vae, R.drawable.vae, R.drawable.vae, R.drawable.vae,
             R.drawable.vae };
-    
+
     private String[] names = { "生命01号", "生命02号", "生命03号", "生命04号", "生命05号",
             "生命06号", "生命07号", "生命08号", "生命09号", "生命10号", "生命11号", "生命12号" };
+
+    @Override
+    public void getRoomsSuccess(RoomsModel roomsModel) {
+        //Logger.json(JSON.toJSONString(roomsModel));
+        Log.d("dd","<<<<<>>>>>>>>>5555");
+        Log.d("dd", JSON.toJSONString(roomsModel));
+        System.out.println(roomsModel);
+    }
 
     //自定义适配器
     class MyAdapter extends BaseAdapter {
         public int getCount() {
             return names.length;
         }
-
         public Object getItem(int position) {
             return names[position];
         }
-
         public long getItemId(int position) {
             System.out.println("<<<<<>>>>>>>>>");
-            System.out.println(position);
             return position;
         }
 
